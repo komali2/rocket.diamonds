e29b550e22526362fa63e604905c0dc27c35f596
Caleb Rogers <caleb@calebjay.com>
2021-01-27 10:52:22 +0800

    Make it work on phones lol

---
+++ gainzcomputer.js
@@ -0,0 +1,36 @@
+const FATTYGAINZ = 1000;
+window.addEventListener('DOMContentLoaded', (event) => {
+  const countInput = document.querySelector('#stocks');
+  const priceInput = document.querySelector('#price');
+  const calculateButton = document.querySelector('#calculate');
+
+  const wealth = document.querySelector('#wealth');
+  let countValue = 0;
+  let priceValue = 0;
+
+  countInput.addEventListener('input', handleCountInput);
+  priceInput.addEventListener('input', handlePriceInput);
+  calculateButton.addEventListener('click', calculateAll);
+  calculateButton.addEventListener('touchstart', calculateAll);
+
+  function calculateAll() {
+    handleCountInput();
+    handlePriceInput();
+  }
+  function handleCountInput(event) {
+    let value = countInput.value;
+    value = Number(value);
+    countValue = value;
+    handleGenericInput();
+  }
+  function handlePriceInput(event) {
+    let value = priceInput.value;
+    value = Number(value);
+    priceValue = value;
+    handleGenericInput();
+  }
+  function handleGenericInput(event) {
+      const out = (countValue * FATTYGAINZ) - (countValue * priceValue);
+      wealth.innerHTML = out;
+  }
+});
